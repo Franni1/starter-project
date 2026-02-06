9aced540020cc6a9494045dc7b417e3a88b6b4ed
Franni1 <annaloro860.fa@gmail.com>
2026-02-06 23:41:11 +0100

    manifest.json

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,21 @@
-self.addEventListener("install", (event) => {
+const CACHE_NAME = 'clock-cache-v1';
+const FILES_TO_CACHE = [
+  './',
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open("clock-cache").then((cache) => {
-      return cache.addAll([
-        "./",
-        "./index.html",
-        "./manifest.json"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
   );
 });
 
-self.addEventListener("fetch", (event) => {
+self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
+    caches.match(event.request).then(response => {
       return response || fetch(event.request);
     })
   );
